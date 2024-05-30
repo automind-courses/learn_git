a16b73e4c43c5d1458813fde57330d83273eba83
nadya <WW@KK.OO>
2024-05-30 20:56:29 +0300

    new print function

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -2,6 +2,12 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("hello!!");
+
+        printMyMessage("KKK");
+    }
+
+    private static void printMyMessage(String kkk) {
+        System.out.println(kkk);
     }
 
 }
