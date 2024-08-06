b23144b99e37555ad0cc0a254222223eefcadaf1
chani.mallot <xvgtt@4587>
2024-08-06 21:33:48 +0300

    System.out.println("שיעור אחרוןןןןןןןןןןןןןןן")

--- src/Maly.java
+++ src/Maly.java
@@ -2,6 +2,7 @@ public class Maly {
     public static void main(String[] args) {
         System.out.println("בייי לכווווווווולם!!!!!!!");
         System.out.println("מירי, אני מכניסה לך אחלה שינוי");
+        System.out.println("שיעור אחרוןןןןןןןןןןןןןןן");
     }
-   System.out.println("שיעור אחרוןןןןןןןןןןןןןןן")
+
 }
--- src/Nadya.java
+++ src/Nadya.java
@@ -1,2 +1,6 @@
 public class Nadya {
+    public static void main(String[] args) {
+        System.out.println("היה כיף איתך,היית מורה מעולה!!!!!!!!!!");
+    }
+
 }
