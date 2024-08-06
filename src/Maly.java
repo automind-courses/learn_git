1ca9df74f31e40c366d44680c38ef4e00fe786f3
maly <m@hj.l>
2024-08-06 21:17:59 +0300

    mali

--- src/Maly.java
+++ src/Maly.java
@@ -1,5 +1,6 @@
 public class Maly {
     public static void main(String[] args) {
         System.out.println("בייי לכווווווווולם!!!!!!!");
+        System.out.println("מירי, אני מכניסה לך אחלה שינוי");
     }
 }
