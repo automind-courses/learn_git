44a7df5ba67eba3996f5003fbb8dc4d8702deb61
chani.mallot <xvgtt@4587>
2024-08-06 21:22:46 +0300

    System.out.println("שיעור אחרוןןןןןןןןןןןןןןן")

--- src/Maly.java
+++ src/Maly.java
@@ -2,4 +2,5 @@ public class Maly {
     public static void main(String[] args) {
         System.out.println("בייי לכווווווווולם!!!!!!!");
     }
+   System.out.println("שיעור אחרוןןןןןןןןןןןןןןן")
 }
